277df74946f0ddbab57ea56d80c6609bbe3b16c6
Nimra991 <nimranaeem.alphasquad@gmail.com>
2023-10-25 17:03:53 +0500

    Added one new practice file

---
+++ string_reversal.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+
+reversed_name = name[::-1]
+# Reverse the string use index
+
+print("Original Name:", name)
+print("Reversed Name:", reversed_name)
+# Print the both original with reversed string
